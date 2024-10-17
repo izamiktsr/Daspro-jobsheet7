bb4820f1173305c424a10c241d9d557169302095
izamiktsr <mazibackup2@gmail.com>
2024-10-17 20:02:57 +0700

    new fix code

--- SiakadWhile18.java
+++ SiakadWhile18.java
@@ -9,9 +9,9 @@ public class SiakadWhile18 {
         System.out.println("Masukkan jumlah mahasiswa: ");
         jml = sc.nextInt();
     
-      int i = 1;
+      int i = 0;
     while (i < jml)   {
-    System.out.println("Masukkan nilai mahaiswa ke-" + (i +1) + ": ");
+    System.out.println("Masukkan nilai mahaiswa ke-" + (i + 1) + ": ");
     int nilai = sc.nextInt();
 
     if (nilai < 0 || nilai > 100) { 
@@ -30,9 +30,9 @@ public class SiakadWhile18 {
     } else if (nilai > 50 && nilai <= 60) { 
         System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
     } else if (nilai > 39 && nilai <= 50) { 
-        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
-    } else if (nilai <= 39) {
         System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+    } else {
+        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
     }
     i++;
 }
