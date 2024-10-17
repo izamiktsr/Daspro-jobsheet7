9a8da635098a9856646c349a9c04a66c760ef5a2
izamiktsr <mazibackup2@gmail.com>
2024-10-17 19:14:26 +0700

    new file here

---
+++ SiakadWhile18.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class SiakadWhile18 {
+    private static int jml;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan jumlah mahasiswa: ");
+        jml = sc.nextInt();
+    
+      int i = 1;
+    while (i < jml)   {
+    System.out.println("Masukkan nilai mahaiswa ke-" + (i +1) + ": ");
+    int nilai = sc.nextInt();
+
+    if (nilai < 0 || nilai > 100) { 
+        System.out.println("Nilai tidak valid. Masukkan lagi nilai yang valid !");
+        continue;
+    }
+
+    if (nilai > 80 && nilai <= 100) {
+        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+    } else if (nilai > 73 && nilai <= 80) {
+        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
+    } else if (nilai >65 && nilai <= 73) {
+        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B");
+    } else if (nilai > 60 && nilai <= 65) { 
+        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C+");
+    } else if (nilai > 50 && nilai <= 60) { 
+        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah C");
+    } else if (nilai > 39 && nilai <= 50) { 
+        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
+    } else if (nilai <= 39) {
+        System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah D");
+    }
+    i++;
+}
+    }
+}
+    
+
+            
+            
+            
+    
+    
+
